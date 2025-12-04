2333a2e6dee058a407863155947b93e2f80be5b0
LuferovAA <163550675+LuferovAA@users.noreply.github.com>
2025-12-04 19:28:18 +0500

    fix: исправлена ошибка в тесте average

--- test_calculator.py
+++ test_calculator.py
@@ -7,5 +7,5 @@ def test_subtract():
     assert calculator.subtract(5, 2) == 3
 
 def test_average():
-    """НАМЕРЕННО НЕПРАВИЛЬНЫЙ ТЕСТ"""
-    assert calculator.average([1, 2, 3]) == 10  # Должно быть 2, а не 10!
+    """Исправленный тест"""
+    assert calculator.average([1, 2, 3]) == 2  # Теперь правильно!
